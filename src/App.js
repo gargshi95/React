837eaaa892b402a1aeae15e1520c0f137f1c4f14
Ayushi Garg <gargshi867@gmail.com>
2025-03-06 23:05:58 +0530

    Changes in src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first demo project for react.
+          This is my first demo project for Docker tutorial ************* Love Docker
         </p>
         <a
           className="App-link"
